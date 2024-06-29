16f60832c398b88d00123ca94452c7ea0a552222
Anesthesiade4cordas <guilhermecqpro@gmail.com>
2024-06-28 23:46:14 -0300

    first commit

---
+++ parescomaray.js
@@ -0,0 +1,25 @@
+// numero %2=== 0 - PAR
+// Numero %2 !==0 - IMPAR
+
+
+
+let numerospares=[0,2,4,6,8,]
+let numerosimpares=[1,3,5,7,9]
+for (let I = 0 ; I<10; I++) {
+    if(I%2===0){ numerospares.push(I)
+    
+    } else{ numerosimpares.push(I)
+
+
+
+    }
+
+console.log(`Numeros pares
+    ${numerospares}`)
+    console.log(`Numeros Impares ${numerosimpares}  `            )
+    
+
+
+
+}
+
---
+++ somacomaray.js
@@ -0,0 +1,6 @@
+let numeros = [60,90,75,100]
+let numeros2 = [0]+[1]+[2]+[3]
+console.log(numeros2)
+for(let i =0;i< numeros.length;i++){
+ numeros[i]
+} console.log(numeros.length)
